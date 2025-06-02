080c79e46fd6b401a4803f7d1423188706cc5a72
Matias Zelarayan <mgzelarayan@comunidad.unnoba.edu.ar>
2025-06-02 11:02:31 -0300

    Cambio tramiento de tipos primitivos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState, type JSX } from "react"
 import { Todos } from "./components/Todos"
+import type { TodoId, TodoTitle } from "./types"
 
 const mockTodos = [
   {
@@ -23,7 +24,7 @@ const mockTodos = [
 const App = (): JSX.Element => {
   const [todos, setTodos] = useState(mockTodos)
 
-  const handleRemove = (id: string): void => {
+  const handleRemove = ({id}: TodoId): void => {
     const newTodos = todos.filter(todo => todo.id !== id)
     setTodos(newTodos)
   }
