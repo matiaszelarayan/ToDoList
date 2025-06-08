104276995589c6a5439f9541b3f720ef97994913
Matias Zelarayan <mgzelarayan@comunidad.unnoba.edu.ar>
2025-06-08 01:31:51 -0300

    filtrar TODOs y mostrar TODOs pendientes

---
+++ src/components/Footer.tsx
@@ -0,0 +1,31 @@
+import { Filters } from "./Filters"
+import { type FilterValue } from "../types"
+
+interface Props{
+    activeCount: number
+    completedCount: number
+    filterSelected: FilterValue
+    onClearCompleted: () => void
+    handleFilterChange: (filter: FilterValue) => void
+}
+
+export const Footer: React.FC<Props> = ({ 
+    activeCount = 0,
+    completedCount = 0,
+    filterSelected,
+    handleFilterChange,
+    onClearCompleted
+}) => {
+    return (
+        <footer className="footer">
+            <span className="todo-count">
+                <strong>{activeCount}</strong> tareas pendientes
+            </span>
+
+            <Filters
+                filterSelected={filterSelected}
+                onFilterChange={() => {}}
+            />
+        </footer>
+    )
+}
